34cf1fad588a77f31ed3c447011f6490dc7b4b7e
MahaboobBasha <sb9333171@gmail.com>
2023-01-08 15:15:38 +0000

    practice on authentication part-one

---
+++ index.js
@@ -0,0 +1,110 @@
+const express = require("express");
+const path = require("path");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const bcrypt = require("bcrypt");
+
+const app = express();
+app.use(express.json());
+const dbPath = path.join(__dirname, "goodreads.db");
+
+let db = null;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server Running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+initializeDBAndServer();
+
+// Get Books API
+app.get("/books/", async (request, response) => {
+  const getBooksQuery = `
+  SELECT
+    *
+  FROM
+    book
+  ORDER BY
+    book_id;`;
+  const booksArray = await db.all(getBooksQuery);
+  response.send(booksArray);
+});
+
+// create user API
+app.post("/users/", async (request, response) => {
+  const { name, username, password, gender, location } = request.body;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const selectUserQuery = `
+  SELECT 
+    * 
+  FROM 
+    user 
+  WHERE 
+    username = '${username}'`;
+
+  const dbUser = await db.get(selectUserQuery);
+
+  if (dbUser === undefined) {
+    // create user table
+
+    const createUserQuery = `
+  INSERT INTO
+    user (username, name, password, gender, location)
+  VALUES
+    (
+      '${username}',
+      '${name}',
+      '${hashedPassword}',
+      '${gender}',
+      '${location}'  
+    );`;
+    await db.run(createUserQuery);
+    response.send("user created successfully");
+  } else {
+    // send invalid username as response
+    response.status(400);
+    response.send("user already exits");
+  }
+});
+
+// user login APi
+app.post("/login/", async (request, response) => {
+  const { username, password } = request.body;
+
+  const selectUserQuery = `
+  SELECT 
+    * 
+  FROM 
+    user 
+  WHERE 
+    username = '${username}'`;
+  const dbUser = await db.get(selectUserQuery);
+
+  if (dbUser === undefined) {
+    // user doesn't exits
+    response.status(400);
+    response.send("invalid user");
+  } else {
+    // compare password , hashed password
+
+    const isPasswordCheck = await bcrypt.compare(password, dbUser.password);
+
+    if (isPasswordCheck === true) {
+      response.send("login success");
+    } else {
+      response.status(400);
+      response.send("invalid password");
+    }
+  }
+});
